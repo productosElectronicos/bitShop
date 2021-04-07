bf45bfd7fa3731bb8095d85bff1491a7dd2a8908
crmejiam <crmejiam@unal.edu.co>
2021-04-06 23:21:03 -0500

    Estructura inicial para metodo ObtenerElementoGuardado
    Aún no se han realizado pruebas ni verificaciones, solo es una estructura inicial

--- imports/api/elementosGuardados/obtenerElementoGuardado.js
+++ imports/api/elementosGuardados/obtenerElementoGuardado.js
@@ -1,5 +1,15 @@
-const obtenerElementoGuardado = (params) => {
-  return 'test';
+import ElementosGuardados from '../../collections/elementosGuardados';
+
+/**
+ * función para obtener los elementos guardados del usuario
+ * @param {String} entrada id del usuario
+ * @returns {Producto[]}
+ */
+
+const obtenerElementoGuardado = (usuarioId) => {
+  const elementosGuardados = ElementosGuardados.find({ usuarioId }).fetch();
+
+  return elementosGuardados;
 };
 
 export default obtenerElementoGuardado;
