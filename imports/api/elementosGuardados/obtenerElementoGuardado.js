b4f0b2c8f7d0a06664c722493461f614ee278f79
crmejiam <crmejiam@unal.edu.co>
2021-04-07 14:16:07 -0500

    Agregar nuevas fucionalidades a obtenerElementoGuardado.js
    Se agregó un límite para la petición, los resultados serán retornados en orden descendente según la fecha de guardado,
    y el resultado serán únicamente los datos del producto, excluyendo el id del usuario y la fecha de guardado

--- imports/api/elementosGuardados/obtenerElementoGuardado.js
+++ imports/api/elementosGuardados/obtenerElementoGuardado.js
@@ -2,12 +2,18 @@ import ElementosGuardados from '../../collections/elementosGuardados';
 
 /**
  * función para obtener los elementos guardados del usuario
- * @param {String} entrada id del usuario
+ * @param {Object} entrada
+ * @param {String} entrada.usuarioId id del usuario
+ * @param {Number} entrada.limit limite de la consulta
  * @returns {Producto[]}
  */
 
-const obtenerElementoGuardado = (usuarioId) => {
-  const elementosGuardados = ElementosGuardados.find({ usuarioId }).fetch();
+const obtenerElementoGuardado = ({ usuarioId, limit = 10, sort = { fechaGuardado: -1 } }) => {
+  const elementosGuardados = ElementosGuardados.find({ usuarioId }, {
+    limit,
+    sort,
+    fields: { usuarioId: 0, fechaGuardado: 0 },
+  }).fetch();
 
   return elementosGuardados;
 };
