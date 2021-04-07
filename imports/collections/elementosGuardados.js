337ef8345aed1ae2a7a6821c1eba664e6f72f14e
JuanJoUN <jusalazarc@unal.edu.co>
2021-04-07 00:21:07 -0500

    Actualizar crearElementoGuardado

--- imports/collections/elementosGuardados.js
+++ imports/collections/elementosGuardados.js
@@ -5,7 +5,7 @@ const ElementosGuardados = new Mongo.Collection('elementosGuardados');
 
 ElementosGuardados.indexes = [
   { index: { usuarioId: 1 } },
-  { index: { url: 1 } },
+  { index: { url: 'url' } },
 ];
 
 export default ElementosGuardados;
