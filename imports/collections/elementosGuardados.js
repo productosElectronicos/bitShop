631352bd70004adde7eb78d75125e930f383b704
crmejiam <crmejiam@unal.edu.co>
2021-04-07 11:40:14 -0500

    actualizar índices de la colección elementos guardados

--- imports/collections/elementosGuardados.js
+++ imports/collections/elementosGuardados.js
@@ -5,7 +5,9 @@ const ElementosGuardados = new Mongo.Collection('elementosGuardados');
 
 ElementosGuardados.indexes = [
   { index: { usuarioId: 1 } },
-  { index: { url: 1 } },
+  { index: { fechaGuardado: 1 } },
+  //  { index: { productoId: 1 } },   cada elemento guardado debe incluir productoId
+  //  { index: { enlaceProducto: 1 } }, cada elemento guardado debe incluir enlaceProducto (url)
 ];
 
 export default ElementosGuardados;
