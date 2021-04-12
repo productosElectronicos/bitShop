56245025197bd20b33d5052d78be9e6f2552d97e
JuanJoUN <jusalazarc@unal.edu.co>
2021-04-11 20:38:26 -0500

    Actualización crearElementoGuardado

--- imports/api/elementosGuardados/crearElementoGuardadoMethod.js
+++ imports/api/elementosGuardados/crearElementoGuardadoMethod.js
@@ -1,17 +1,37 @@
+import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import SimpleSchema from 'simpl-schema';
 
 import crearElementoGuardado from './actualizarElementoGuardado';
 
-const crearElementoGuardadoMethod = new ValidatedMethod({
+const crearElementoGuadradoMetodo = new ValidatedMethod({
   name: 'crearElementoGuardado',
   validate: new SimpleSchema({
-    nombre: { type: String },
-    numeroTelefonico: {
-      type: Number,
-      optional: true,
-    },
-  }),
+    producto: { type: Object },
+    'producto.Id':{ type: String },
+    'usuario.Id': { type: String },
+    'fechaGuardado':{ type: Date },
+    'producto.enlaceProducto': { type: String },
+  }).validator(),
+  run({ producto }) {
+    const { _id: usuarioId } = Meteor.user();
+
+    const elementoGuardado = crearElementoGuardado({ producto, usuarioId });
+
+    return elementoGuardado;
+  },
 });
 
-export default crearElementoGuardadoMethod;
+export default crearElementoGuardadoMetodo;
+
+
+
+
+
+
+
+
+
+
+
+export default crearElementoGuardadoMetodo;
