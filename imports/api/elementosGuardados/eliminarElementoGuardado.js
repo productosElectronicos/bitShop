8d027dea3ad4b2520946b04d60dfd7ccd915a25d
Sebastián <juansebasmorenoruiz@gmail.com>
2021-04-14 19:20:32 -0500

    doc eliminar elemento guardado

--- imports/api/elementosGuardados/eliminarElementoGuardado.js
+++ imports/api/elementosGuardados/eliminarElementoGuardado.js
@@ -1,5 +1,28 @@
 import ElementosGuardados from '../../collections/elementosGuardados';
 
-const eliminarElementoGuardado = (element) => ElementosGuardados.remove(element).fetch();
+/**
+ * objeto con datos de producto guardado
+ * @typedef Producto
+ * @property {String} nombreProducto
+ * @property {Number} precioProducto
+ * @property {String} descripcionProducto
+ * @property {String} localizacion
+ * @property {String} fotoProducto
+ * @property {Boolean} esUsado
+ * @property {String} tienda
+ * @property {String} enlaceProducto
+ * @property {String} usuarioId
+ * @property {Date} fechaGuardado
+ */
+
+/**
+ * función para obtener los elementos guardados del usuario
+ * @param {Object} entrada
+ * @param {String} entrada.usuarioId id del usuario
+ * @param {Number} entrada.productoId id del producto
+ * @returns {Producto[]}
+ */
+
+const eliminarElementoGuardado = (entrada) => ElementosGuardados.remove(entrada).fetch();
 
 export default eliminarElementoGuardado;
