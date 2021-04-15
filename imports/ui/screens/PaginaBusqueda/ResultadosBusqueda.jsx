8d756bd58ea25733618cf0acf31b66af259d6c77
Juan Diego Marín Rodríguez <jumarinr@unal.edu.co>
2021-04-15 13:35:04 -0500

    prueba anuncios

--- imports/ui/screens/PaginaBusqueda/ResultadosBusqueda.jsx
+++ imports/ui/screens/PaginaBusqueda/ResultadosBusqueda.jsx
@@ -19,6 +19,23 @@ const ResultadosBusqueda = ({ resultados }) => (
 
       <Grid item xs={12} md={10}>
         <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+            {/* <!-- Anuncios busquedas --> */}
+            <ins
+              className="adsbygoogle"
+              style={{ display: 'block' }}
+              data-ad-client="ca-pub-2286863170591933"
+              data-ad-slot="6008623895"
+              data-ad-format="auto"
+              data-full-width-responsive="true"
+            />
+            <script>
+              (adsbygoogle = window.adsbygoogle || []).push(
+              {}
+              );
+            </script>
+          </Grid>
           {resultados.map((resultado) => (
             <Grid item xs={12} md={3} key={resultado.enlaceProducto}>
               <CardResultado {...resultado} />
