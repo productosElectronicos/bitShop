c7765a2c7e85a241edf226360df148eefa9a75ce
Juan Toro <juanalbtoro@hotmail.com>
2021-04-27 20:49:08 -0500

    pequeña correccion y prueba

--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
 import '../imports/startup';
 
 import { Meteor } from 'meteor/meteor';
+import crearElementoGuardado from '../imports/api/elementosGuardados/crearElementoGuardado';
 
 Meteor.startup(() => {
-
+  crearElementoGuardado({ producto: { enlaceProducto: 'http://blabla' }, usuarioId: 1 });
 });
